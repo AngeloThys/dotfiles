ffd5bb03cd76530d56230eb8bb9b82b1e1cf7eae
athys <thys.angelo@gmail.com>
2024-01-24 17:51:24 +0000

    Modify lazyvim ansible tasks

--- home/run_once_install_ansible.sh
+++ home/run_once_install_ansible.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt-get install ansible
+sudo apt-get install -y ansible
 
 echo "Ansible installation complete"
 
