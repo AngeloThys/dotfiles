46b4f86f0e980075468fa45baaf132ac82356ab8
athys <thys.angelo@gmail.com>
2024-01-24 15:28:01 +0000

    Modify ansible setup script

--- home/run_once_install_ansible.sh
+++ home/run_once_install_ansible.sh
@@ -5,6 +5,8 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible
 
+echo "Ansible installation complete"
+
 ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
 
-echo "Ansible installation complete"
+echo "System setup complete"
