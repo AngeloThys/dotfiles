5492955e6f90c9b8d1a901b6962b8adf3700d708
athys <thys.angelo@gmail.com>
2024-02-05 11:42:47 +0000

    Add home/.config/zsh/ohmyzsh/custom/aliases.zsh

---
+++ home/dot_config/zsh/ohmyzsh/custom/aliases.zsh
@@ -0,0 +1,8 @@
+# Custom aliases
+alias code='code-insiders'
+alias lsa='lsd -ah'
+alias lsl='lsd -lah'
+alias ls='lsd'
+alias vim='nvim'
+alias celar='clear'
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
