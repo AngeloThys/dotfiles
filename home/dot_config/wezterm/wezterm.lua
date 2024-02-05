9c17e05f82bb305b0f04b6e27c99254a9da8c7be
athys <thys.angelo@gmail.com>
2024-02-05 09:19:28 +0000

    Add home/.config/wezterm/wezterm.lua

---
+++ home/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Tokyo Night Moon",
+	font = wezterm.font("FiraCode Nerd Font"),
+	font_dirs = { "/home/athys/.local/share/fonts/" },
+	window_padding = {
+		left = "0",
+		right = "0",
+		top = "0",
+		bottom = "0",
+	},
+	use_fancy_tab_bar = false,
+	audible_bell = "Disabled",
+}
