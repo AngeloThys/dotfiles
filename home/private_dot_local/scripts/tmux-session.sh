e14d868dbb315d92e613d7302f621be8616464f8
athys <thys.angelo@gmail.com>
2024-02-05 09:06:50 +0000

    Add home/.local/scripts/tmux-session.sh

---
+++ home/private_dot_local/scripts/tmux-session.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+session="og"
+# check if in tmux session
+if [ -n "$TMUX" ]; then
+	current_session=$(tmux display-message -p '#S')
+	if [ "$current_session" = "$session" ]; then
+		echo "Already attached to $session"
+	else
+		tmux switch-client -t $session
+	fi
+else
+	tmux has-session -t $session 2>/dev/null
+	if [ $? != 0 ]; then
+		tmux new -s $session
+	else
+		tmux attach-session -t $session
+	fi
+fi
